e0201efbd054ff6f7c664b2978538d75ec229e0f
baibobob <boliang@umich.edu>
2016-08-04 14:05:46 +0800

    Add files via upload

---
+++ simple_io.java
@@ -0,0 +1,14 @@
+
+import java.io.*;
+public class simple_io {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		
+		BufferedReader keybd =
+				new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(keybd.readLine());
+	
+	}
+
+}
