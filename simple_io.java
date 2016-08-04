6c2bd76aa5f8188acdefc843d8c8c26db5f9c15c
baibobob <boliang@umich.edu>
2016-08-04 14:06:49 +0800

    Update simple_io.java

--- simple_io.java
+++ simple_io.java
@@ -1,14 +1,11 @@
 
 import java.io.*;
 public class simple_io {
-
 	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
 		
 		BufferedReader keybd =
 				new BufferedReader(new InputStreamReader(System.in));
 		System.out.println(keybd.readLine());
-	
 	}
-
 }
